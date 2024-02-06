bdb31283a62775ea9282b7bc0af37dafeb918bec
CHizzyEzu <155769930+CHizzyEzu@users.noreply.github.com>
2024-02-06 18:50:29 +0000

    Changed metric system

--- Avg.cpp
+++ Avg.cpp
@@ -8,15 +8,15 @@ int main()
     double mph, hours, distance;
     
     // Avg Rate of Speed of Vehicle
-    cout << "How fast are you going on Average (mph)? ";
+    cout << "How fast are you going on Average (Km)? ";
     cin >> mph;
 
     //Distance from destination
-    cout << "How far are you from your destination (miles)? ";
+    cout << "How far are you from your destination (Kilometers)? ";
     cin >> distance;
 
     // Calculate Avg Time to destination
-    hours = distance / mph;
+    hours = distance / km;
 
     // Display avg time 
     cout << "Your average speed indicates you will arrive at your destination in this many hours " << hours << endl;
