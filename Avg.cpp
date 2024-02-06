471ced9224187c0b4f22ba616145f8cc2479d1a5
CHizzyEzu <155769930+CHizzyEzu@users.noreply.github.com>
2024-02-06 07:35:09 -0500

    Create Avg.cpp

---
+++ Avg.cpp
@@ -0,0 +1,24 @@
+// Calculating Hours to Destination
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double mph, hours, distance;
+    
+    // Avg Rate of Speed of Vehicle
+    cout << "How fast are you going on Average? ";
+    cin >> mph;
+
+    //Distance from destination
+    cout << "How far are you from your destination? ";
+    cin >> distance;
+
+    // Calculate Avg Time to destination
+    hours = distance / mph;
+
+    // Display avg time 
+    cout << "Your average speed indicates you will arrive at your destination in this many hours " << hours << endl;
+    return 0;
+}
